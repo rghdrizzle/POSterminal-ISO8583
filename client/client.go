f1701ca039e23ee4b3f8611f71b59b2f658a2852
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2024-04-30 22:35:56 +0530

    code: created function to handle connection

--- client/client.go
+++ client/client.go
@@ -3,11 +3,33 @@ package main
 import (
     "net"
     "fmt"
+    "bufio"
+    "os"
   )
 
 func main(){
-  _, err:= net.Dial("tcp","localhost:8080")
+  conn, err:= net.Dial("tcp","localhost:8080")
   if err!=nil{
     fmt.Println(err)
+  }
+  defer conn.Close()
+  for {
+    fmt.Println("Enter the message to send:")
+    scanner:=bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    msg:=scanner.Text()
+    _,err:= conn.Write([]byte(msg))
+    if err!=nil{
+      fmt.Println(err)
+    }
+
+     buf := make([]byte, 1024)
+    _, err= conn.Read(buf)
+    if err!=nil{
+      fmt.Println(err)
+    }
+    fmt.Println(string(buf))
+
+    
   }
 }
--- server.go
+++ server.go
@@ -4,6 +4,8 @@ import (
   "net"
   "log"
   "fmt"
+  "bufio"
+  "os"
 )
 
 func StartServer(){
@@ -12,13 +14,35 @@ func StartServer(){
       log.Fatal(err)
     }
     for {
-      _, err := ln.Accept();
+      conn, err := ln.Accept();
       if err!=nil{
         log.Fatal(err)
       }
       fmt.Println("Connected")
       
-      // run go routine for handling connection
+      go handleConnection(conn)
     }
 
+}
+
+func handleConnection(conn net.Conn){
+  defer conn.Close()
+  for{
+    buf := make([]byte, 1024)
+    _, err:= conn.Read(buf)
+    if err!=nil{
+      fmt.Println(err)
+    }
+    fmt.Println("Message received from client:"+string(buf))
+
+    fmt.Println("Enter the message to send:")
+    scanner:=bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    msg:=scanner.Text()
+    _,err = conn.Write([]byte(msg))
+    if err!=nil{
+      fmt.Println(err)
+    }
+
+  }
 }
